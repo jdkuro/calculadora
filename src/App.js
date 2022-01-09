75255b1ad7ce53a732c86b8bf1fbb56352c1d24e
David Murillo <jdmurillo1995@gmail.com>
2022-01-08 23:55:35 -0600

    Agregar apellido al componente App

--- src/App.js
+++ src/App.js
@@ -4,24 +4,37 @@ import TextField from "@mui/material/TextField";
 
 function App() {
   const [nombre, setNombre] = useState("");
+  const [apellido, setApellido] = useState("");
 
   const asignarNombre = (event) => {
     setNombre(event.target.value);
   };
 
+  const asignarApellido = (event) => {
+    setApellido(event.target.value);
+  };
+
   const mostarNombre = () => {
-    alert("Hola soy " + nombre);
+    alert("Hola soy " + nombre + " " + apellido);
   };
 
   return (
     <div style={{ margin: 30 }}>
-      <p>{nombre}</p>
+      <p>
+        {nombre} {apellido}
+      </p>
       <br />
       <TextField
         label="Ingrese su nombre"
         variant="standard"
         value={nombre}
         onChange={asignarNombre}
+      />
+      <TextField
+        label="Ingrese su apellido"
+        variant="standard"
+        value={apellido}
+        onChange={asignarApellido}
       />
       <Button variant="contained" onClick={mostarNombre}>
         Mostrar nombre
